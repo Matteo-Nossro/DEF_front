30d6c34c50262e2d1d8625bf64579c715eae965f
Sebastien Gaillard <sebastien.gaillard@acedigitale.com>
2023-12-21 08:47:17 +0100

    Avanr d'ajouter le repo dans le service

--- DFV-Project/src/main/java/com/example/MSCE/DFVProject/repository/TransactionImmobiliereRepository.java
+++ DFV-Project/src/main/java/com/example/MSCE/DFVProject/repository/TransactionImmobiliereRepository.java
@@ -2,8 +2,15 @@ package com.example.MSCE.DFVProject.repository;
 
 import com.example.MSCE.DFVProject.business.TransactionImmobiliere;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionImmobiliereRepository extends JpaRepository<TransactionImmobiliere, Long> {
 
-        TransactionImmobiliere findByCodeCommuneAndCodePostal(String codeCommune, String codePostal);
+        List<TransactionImmobiliere> findByCodeCommuneAndCodePostal(String codeCommune, String codePostal);
+        @Query("SELECT t FROM TransactionImmobiliere t WHERE t.longitude BETWEEN :minLon AND :maxLon AND t.latitude BETWEEN :minLat AND :maxLat")
+        List<TransactionImmobiliere> findByLocationWithin(@Param("minLon") Double minLon, @Param("maxLon") Double maxLon, @Param("minLat") Double minLat, @Param("maxLat") Double maxLat);
+
 }
