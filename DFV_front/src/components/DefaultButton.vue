bce7d76c3ed02920d0b280ceb99a048a39fde5ca
matteo <matnoss@gmail.com>
2023-12-21 12:56:34 +0100

    feat: ajout get curent localisation + fly to + formulaire qui remarche

--- DFV_front/src/components/DefaultButton.vue
+++ DFV_front/src/components/DefaultButton.vue
@@ -1,16 +1,19 @@
 
 <template>
-  <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" :style="props.styles" type="submit" :value="props.label" @click="props.click">
+  <input class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" :style="props.styles" type="submit" :value="props.label" @click="props.click" v-if="props.type==='input'"/>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" :style="props.styles"  @click="props.click" v-if="props.type==='button'">{{props.label}}</button>
 </template>
 <script setup lang="ts">
 
 import {defineProps} from "vue";
 
 const props = defineProps({
-  click : MouseEvent,
+  click : Function as () => void,
   label : String,
   styles : String || null,
+  type : String || null
 })
+
 </script>
 
 <style scoped lang="scss">
